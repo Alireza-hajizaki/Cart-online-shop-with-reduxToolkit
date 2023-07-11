cc5fa2f0010ac1eb6c4d07f8b3a03c9b748fbc1e
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-12 00:58:46 +0330

    header created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useRoutes} from 'react-router-dom';
 import routes from './routes';
+import Navbar from './components/Navbar';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar/>
       {router}
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Main from '../components/Main'
+
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <Main/>
   )
 }
 
