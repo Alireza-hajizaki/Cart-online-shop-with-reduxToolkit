3a56b8c09129bb7f790034944d05e081c71a4845
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-12 03:56:37 +0330

    Async Thunk & axios Fetch created

--- src/Redux/slices/productsSlice.js
+++ src/Redux/slices/productsSlice.js
@@ -1,14 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios"
 
 const initialState = {
     items : [],
-    status: null
+    status: null,
+    // error: null,
 }
+
+export const productsFetch = createAsyncThunk(
+    "products/productsFetch",
+     async () => {
+        const response = await axios.get("http://localhost:5000/products")
+        return response?.data
+     }
+    // async(id= null , {rejectWithValue}) => {
+    //     try{
+    //         const response = await axios.get("http://localhost:5000/productss")
+    //         return response?.data
+    //     }catch (error){
+    //         return rejectWithValue('an error occured');
+    //         // return rejectWithValue(error.response.data);
+    //     }
+    // }
+);
 
 const productsSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [productsFetch.pending] : (state , action) => {
+            state.status = "pending"
+        },
+        [productsFetch.fulfilled] : (state , action) => {
+            state.status = "success"
+            state.items = action.payload
+        },
+        [productsFetch.rejected] : (state , action) => {
+            state.status = "rejected"
+            // state.error = action.payload
+        },
+    }
 })
 
 
